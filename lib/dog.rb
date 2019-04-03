ea736969de3197de27a85d31d49eaf4ee7bcce60
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-03 23:49:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
 class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all.each do |name|
+      puts "#{name}"
+    end
+  end  
+  
+  
 end
